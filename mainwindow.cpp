24026b1e2cbea146df338bbebca1598edf7407da
Maurycy Liebner <maurycy.liebner@gmail.com>
2016-08-06 16:03:59 +0200

    Added QGraphicsView and QGraphicsScene to MainWindow.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,8 +1,12 @@
 #include "mainwindow.h"
+#include <QtSvg/QGraphicsSvgItem>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    graphicsScene = new QGraphicsScene(this);
+    graphicsView = new QGraphicsView(graphicsScene, this);
+    setCentralWidget(graphicsView);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,11 +2,14 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QGraphicsView>
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
+private:
+    QGraphicsView *graphicsView;
+    QGraphicsScene *graphicsScene;
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
