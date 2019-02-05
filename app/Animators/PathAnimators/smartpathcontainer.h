30a4b81c2ff858a4989056d747a94052c1e99603
Maurycy Liebner <maurycy.liebner@protonmail.com>
2019-02-05 12:15:23 +0100

    Rewriting again.

--- app/Animators/PathAnimators/smartpathcontainer.h
+++ app/Animators/PathAnimators/smartpathcontainer.h
@@ -1,4 +1,4 @@
-#ifndef SMARTPATHCONTAINER_H
+﻿#ifndef SMARTPATHCONTAINER_H
 #define SMARTPATHCONTAINER_H
 #include "simplemath.h"
 #include "skia/skiaincludes.h"
@@ -47,7 +47,7 @@ public:
         MERGE, SPLIT,
         DISCONNECT, CONNECT,
         DISSOLVE, REMOVE_DISSOLVED, ADD_DISSOLVED,
-        NORMALIZE_DISSOLVED
+        NORMALIZE_DISSOLVED, REMOVE_START_OR_END
     };
 
     NodeAction(const Type& type) : mType(type) {
@@ -59,8 +59,41 @@ public:
     }
 
     virtual void apply(QList<Node>& nodes) const = 0;
+    virtual int getMaxInfluencedId() const = 0;
 private:
     Type mType;
+};
+
+class OneNodeAction : public NodeAction {
+public:
+    OneNodeAction(const int& nodeId, const Type& type) :
+        NodeAction(type) {
+        mNodeId = nodeId;
+    }
+
+    int getMaxInfluencedId() const {
+        return mNodeId;
+    }
+protected:
+    int mNodeId;
+};
+
+class TwoNodesAction : public NodeAction {
+public:
+    TwoNodesAction(const int& node1Id,
+                   const int& node2Id,
+                   const Type& type) :
+        NodeAction(type) {
+        mNode1Id = node1Id;
+        mNode2Id = node2Id;
+    }
+
+    int getMaxInfluencedId() const {
+        return mNode2Id;
+    }
+protected:
+    int mNode1Id;
+    int mNode2Id;
 };
 
 //class MergeNodesAction : public NodeAction {
@@ -152,15 +185,21 @@ bool areNodesAdjecent(const int& node1Id, const int& node2Id,
     return node1Id == nodes.count() - 1 && node2Id == 0;;
 }
 
-class DisconnectNodeAction : public NodeAction {
+int fixNegativeId(const int& id, const QList<Node>& nodes) {
+    if(id >= 0) return id;
+    return id + nodes.count();
+}
+
+class DisconnectNodesAction : public TwoNodesAction {
 public:
-    DisconnectNodeAction(const int& node1Id, const int& node2Id) :
-        NodeAction(DISCONNECT), mNode1Id(node1Id), mNode2Id(node2Id) {}
+    //! @brief Node1Id can be negative (ex. -1 will reference last node)
+    DisconnectNodesAction(const int& node1Id, const int& node2Id) :
+        TwoNodesAction(node1Id, node2Id, DISCONNECT) {}
 
     void apply(QList<Node>& nodes) const {
         if(!areNodesAdjecent(mNode1Id, mNode2Id, nodes))
             RuntimeThrow("Cannot disconnect non-adjacent nodes");
-        Node& node1 = nodes[mNode1Id];
+        Node& node1 = nodes[fixNegativeId(mNode1Id, nodes)];
         Node& node2 = nodes[mNode2Id];
         if(!node1.isNormal() || !node2.isNormal())
             RuntimeThrow("Unsupported node type(s)");
@@ -176,20 +215,18 @@ public:
             node2.fType = Node::NORMAL_START;
         }
     }
-private:
-    int mNode1Id;
-    int mNode2Id;
 };
 
-class ConnectNodeAction : public NodeAction {
+class ConnectNodesAction : public TwoNodesAction {
 public:
-    ConnectNodeAction(const int& node1Id, const int& node2Id) :
-        NodeAction(CONNECT), mNode1Id(node1Id), mNode2Id(node2Id) {}
+    //! @brief Node1Id can be negative (ex. -1 will reference last node)
+    ConnectNodesAction(const int& node1Id, const int& node2Id) :
+        TwoNodesAction(node1Id, node2Id, CONNECT) {}
 
     void apply(QList<Node>& nodes) const {
         if(!areNodesAdjecent(mNode1Id, mNode2Id, nodes))
             RuntimeThrow("Cannot connect non-adjacent nodes");
-        Node& node1 = nodes[mNode1Id];
+        Node& node1 = nodes[fixNegativeId(mNode1Id, nodes)];
         Node& node2 = nodes[mNode2Id];
         if(!node1.isNormal() || !node2.isNormal())
             RuntimeThrow("Unsupported node type(s)");
@@ -205,36 +242,56 @@ public:
             node2.fType = Node::NORMAL_MIDDLE;
         }
     }
-private:
-    int mNode1Id;
-    int mNode2Id;
 };
 
-class DissolveNode : public NodeAction {
+class DissolveNode : public OneNodeAction {
 public:
     DissolveNode(const int& nodeId, const qreal& t) :
-        NodeAction(DISSOLVE), mNodeId(nodeId), mT(t) {}
+        OneNodeAction(nodeId, DISSOLVE), mT(t) {}
 
     void apply(QList<Node>& nodes) const {
         Node& node = nodes[mNodeId];
 
         node.fType = Node::DISSOLVED;
         node.fRelT = mT;
-//        Node& prevNode = nodes[mNodeId - 1];
-//        Node& nextNode = nodes[mNodeId + 1];
-//        qCubicSegment2D surrSeg(prevNode.fP1, prevNode.fC2,
-//                                nextNode.fC0, nextNode.fP1);
-//        node.fRelT = surrSeg.tAtPos(node.fP1);
     }
 private:
-    int mNodeId;
     qreal mT;
 };
 
-class RemoveDissolvedNode : public NodeAction {
+//class RemoveStartOrEndNode : public OneNodeAction {
+//    RemoveStartOrEndNode(const int& nodeId) :
+//        OneNodeAction(nodeId, REMOVE_START_OR_END) {}
+
+//    void apply(QList<Node>& nodes) const {
+//        const Node& node = nodes[mNodeId];
+//        if(node.fType != Node::NORMAL_START &&
+//           node.fType != Node::NORMAL_END &&
+//           node.fType != Node::NORMAL_START_AND_END)
+//            RuntimeThrow("RemoveStartOrEndeNode: Unsupported Node Type");
+//        if(node.fType == Node::NORMAL_START) {
+//            Node& nextNode = nodes[mNodeId + 1];
+//            if(nextNode.fType == Node::NORMAL_END) {
+//                nextNode.fType = Node::NORMAL_START_AND_END;
+//            } else {
+//                nextNode.fType = Node::NORMAL_START;
+//            }
+//        } else if(node.fType == Node::NORMAL_END) {
+//            Node& prevNode = nodes[mNodeId - 1];
+//            if(prevNode.fType == Node::NORMAL_START) {
+//                prevNode.fType = Node::NORMAL_START_AND_END;
+//            } else {
+//                prevNode.fType = Node::NORMAL_END;
+//            }
+//        }
+//        nodes.removeAt(mNodeId);
+//    }
+//};
+
+class RemoveDissolvedNode : public OneNodeAction {
 public:
     RemoveDissolvedNode(const int& nodeId) :
-        NodeAction(REMOVE_DISSOLVED), mNodeId(nodeId) {}
+        OneNodeAction(nodeId, REMOVE_DISSOLVED) {}
 
     void apply(QList<Node>& nodes) const {
         const Node& node = nodes[mNodeId];
@@ -255,10 +312,6 @@ public:
             } else {
                 prevNode.fRelT = a*b/div;
             }
-//            Node& prevPrevNode = nodes[mNodeId - 2];
-//            qCubicSegment2D seg(prevPrevNode.fP1, prevPrevNode.fC2,
-//                                nextNode.fC0, nextNode.fP1);
-//            prevNode.fRelT = seg.tAtPos(prevNode.fP1);
         }
         if(nextNode.fType == Node::DISSOLVED) {
             const qreal a = node.fRelT;
@@ -269,28 +322,21 @@ public:
             } else {
                  nextNode.fRelT = b/div;
             }
-//            Node& nextNextNode = nodes[mNodeId + 2];
-//            qCubicSegment2D seg(prevNode.fP1, prevNode.fC2,
-//                                nextNextNode.fC0, nextNextNode.fP1);
-//            nextNode.fRelT = seg.tAtPos(prevNode.fP1);
         }
         nodes.removeAt(mNodeId);
     }
-private:
-    int mNodeId;
 };
 
-class AddDissolvedNode : public NodeAction {
+class InsertDissolvedNode : public OneNodeAction {
 public:
-    AddDissolvedNode(const int& node1Id, const int& node2Id, const qreal& t) :
-        NodeAction(ADD_DISSOLVED), mNode1Id(node1Id), mNode2Id(node2Id), mT(t) {}
+    InsertDissolvedNode(const int& nodeId, const qreal& t) :
+        OneNodeAction(nodeId, ADD_DISSOLVED), mT(t) {}
 
     void apply(QList<Node>& nodes) const {
-        if(mNode1Id + 1 != mNode2Id)
-            RuntimeThrow("Cannot add node between non-adjacent nodes");
-        Node &prevNode = nodes[mNode1Id];
-        Node &nextNode = nodes[mNode2Id];
-        Node newNode;
+        nodes.insert(mNodeId, Node());
+        Node& newNode = nodes[mNodeId];
+        Node &prevNode = nodes[mNodeId - 1];
+        Node &nextNode = nodes[mNodeId + 1];
         newNode.fRelT = mT;
         newNode.fType = Node::DISSOLVED;
         gGetValuesForNodeInsertion(prevNode.fP1, prevNode.fC2,
@@ -304,18 +350,15 @@ public:
             const qreal mNewNodeT = gMapTFromFragment(0, nextNode.fRelT, mT);
             nextNode.fRelT = gMapTToFragment(mNewNodeT, 1, nextNode.fRelT);
         }
-        nodes.insert(mNode2Id, newNode);
     }
 private:
-    int mNode1Id;
-    int mNode2Id;
     qreal mT;
 };
 
-class NormalizeDissolvedNode : public NodeAction {
+class NormalizeDissolvedNode : public OneNodeAction {
 public:
     NormalizeDissolvedNode(const int& nodeId) :
-        NodeAction(NORMALIZE_DISSOLVED), mNodeId(nodeId) {}
+        OneNodeAction(nodeId, NORMALIZE_DISSOLVED) {}
 
     void apply(QList<Node>& nodes) const {
         Node& node = nodes[mNodeId];
@@ -323,13 +366,120 @@ public:
              RuntimeThrow("NormalizeDissolvedNode: Unsupported Node Type");
         node.fType = Node::NORMAL_MIDDLE;
     }
-private:
-    int mNodeId;
 };
+
+SkPath nodesToSkPath(const QList<Node>& nodes) {
+    SkPath result;
+    QPointF lastC2;
+    bool first = true;
+    Node firstMoveNode;
+    for(const Node& node : nodes) {
+        const bool move = first || node.fType == Node::NORMAL_START;
+        if(first) first = false;
+        if(move) {
+            firstMoveNode = node;
+            result.moveTo(qPointToSk(node.fP1));
+            lastC2 = node.fC2;
+            continue;
+        }
+        result.cubicTo(qPointToSk(lastC2),
+                       qPointToSk(node.fC0),
+                       qPointToSk(node.fP1));
+        if(node.fType == Node::NORMAL_END) {
+            result.cubicTo(qPointToSk(node.fC2),
+                           qPointToSk(firstMoveNode.fC0),
+                           qPointToSk(firstMoveNode.fP1));
+        } else {
+            lastC2 = node.fC2;
+        }
+    }
+    return result;
+}
+
+bool actionsCompare(const NodeAction * const node1,
+                    const NodeAction * const node2) {
+    return node1->getMaxInfluencedId() > node2->getMaxInfluencedId();
+}
+
+void sortNodeActions(QList<NodeAction*>& actions) {
+    std::sort(actions.begin(), actions.end(), actionsCompare);
+}
+
+qCubicSegment2D segmentFromNodes(const Node& prevNode,
+                                 const Node& nextNode) {
+    return qCubicSegment2D(prevNode.fP1, prevNode.fC2,
+                           nextNode.fC0, nextNode.fP1);
+}
+
 class ActionsApplier {
 public:
 
+    void prevNodeInserted(const int& nodeId) {
+        InsertDissolvedNode(nodeId, 0.5).apply(mNodesBefore);
+    }
 
+    void prevNodeRemoved(const int& nodeId) {
+        auto& node = mNodesBefore[nodeId];
+        if(node.fType == Node::DISSOLVED) {
+            auto nodeAction = getActionForNode(nodeId);
+            if(nodeAction->getType() == NodeAction::REMOVE_DISSOLVED) {
+                nodeAction->apply(mNodesBefore);
+            } else if(nodeAction->getType() == NodeAction::NORMALIZE_DISSOLVED) {
+                return;
+            }
+            insertAction(new RemoveDissolvedNode(nodeId));
+        }
+        node.fType = Node::DISSOLVED;
+        node.fRelT = 0.5;
+        Node& prevNode = mNodesBefore[nodeId - 1];
+        Node& nextNode = mNodesBefore[nodeId + 1];
+        auto prevNextSeg = segmentFromNodes(prevNode, nextNode);
+        auto divided = prevNextSeg.dividedAtT(node.fRelT);
+        const auto& first = divided.first;
+        const auto& second = divided.second;
+        prevNode.fC2 = first.c1();
+        node.fC0 = first.c2();
+        node.fP1 = first.p1();
+        node.fC2 = second.c1();
+        nextNode.fC0 = second.c2();
+        insertAction(new RemoveDissolvedNode(nodeId));
+    }
+
+    void atNodeRemoved(const int& nodeId) {
+        insertAction(new DissolveNode(nodeId, 0.5));
+    }
+
+    void atNodeInserted(const int& nodeId, const qreal& t) {
+
+        insertAction(new NormalizeDissolvedNode(nodeId));
+    }
+
+private:
+//    void applyNodeAction(const NodeAction * const action) {
+//        action->apply(mNodesBefore);
+//    }
+
+    void insertAction(NodeAction * const action) {
+        mAfterUpToDate = false;
+    }
+
+    NodeAction * getActionForNode(const int& nodeId) {
+        return nullptr;
+    }
+
+    void updateNodesAfter() {
+        mNodesAfter = mNodesBefore;
+        for(const auto& action : mActions) {
+            action->apply(mNodesAfter);
+        }
+        mAfterUpToDate = true;
+    }
+    bool mAfterUpToDate = true;
+
+    QList<Node> mNodesBefore;
+    QList<Node> mNodesAfter;
+
+    QList<NodeAction*> mActions;
 };
 
 #endif // SMARTPATHCONTAINER_H
