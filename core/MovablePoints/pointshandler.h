23fc2b4eec0dc73c1b0eb1bc59be4c13764797fa
Maurycy Liebner <maurycy.liebner@protonmail.com>
2019-04-16 19:21:37 +0200

    Changes.

--- core/MovablePoints/pointshandler.h
+++ core/MovablePoints/pointshandler.h
@@ -8,9 +8,24 @@ class PointsHandler : public StdSelfRef {
 protected:
     PointsHandler();
 public:
-    virtual MovablePoint *getPointAtAbsPos(const QPointF &absPos,
-                                           const CanvasMode &canvasMode,
-                                           const qreal &canvasScaleInv) {
+    virtual void afterCanvasModeChanged(const CanvasMode &mode) {
+        Q_UNUSED(mode);
+    }
+
+    template <class T, typename... Args>
+    T *createNewPt(const int& id, Args && ...args) {
+        const auto newPt = SPtrCreateTemplated(T)(args...);
+        mPts.insert(id, newPt);
+        return newPt.get();
+    }
+
+    template <class T, typename... Args>
+    T *createNewPt(Args && ...args) {
+        return createNewPt<T>(mPts.count(), args...);
+    }
+
+    MovablePoint *getPointAtAbsPos(const QPointF &absPos,
+                                   const qreal &canvasScaleInv) {
         for(int i = mPts.count() - 1; i >= 0; i--) {
             const auto& pt = mPts.at(i);
             if(pt->isHidden()) continue;
@@ -20,17 +35,15 @@ public:
         return nullptr;
     }
 
-    void allPointsForSelection(QList<stdptr<MovablePoint>> &list,
-                               const CanvasMode &currentCanvasMode) {
+    void allPointsForSelection(QList<stdptr<MovablePoint>> &list) {
         for(const auto& pt : mPts) {
             if(pt->isHidden() || !pt->isSelected()) continue;
             if(pt->selectionEnabled()) list << pt.get();
         }
     }
 
-    virtual void pointsInRectForSelection(const QRectF &absRect,
-                                          const CanvasMode &currentCanvasMode,
-                                          QList<stdptr<MovablePoint>> &list) const {
+    void pointsInRectForSelection(const QRectF &absRect,
+                                  QList<stdptr<MovablePoint>> &list) const {
         for(const auto& pt : mPts) {
             if(!pt->selectionEnabled()) continue;
             if(pt->isSelected() && pt->isHidden()) continue;
@@ -38,10 +51,9 @@ public:
         }
     }
 
-    virtual void drawPoints(SkCanvas * const canvas,
-                            const CanvasMode &currentCanvasMode,
-                            const SkScalar &invScale,
-                            const bool& keyOnCurrentFrame) const {
+    void drawPoints(SkCanvas * const canvas,
+                    const SkScalar &invScale,
+                    const bool& keyOnCurrentFrame) const {
         for(int i = mPts.count() - 1; i >= 0; i--) {
             const auto& pt = mPts.at(i);
             if(pt->isVisible()) pt->drawSk(canvas, invScale);
@@ -55,28 +67,14 @@ public:
         return static_cast<T*>(mPts.at(id).get());
     }
 
-    void removeAt(const int& id) {
-        mPts.removeAt(id);
-    }
+    void removeAt(const int& id) { mPts.removeAt(id); }
+    void clear() { mPts.clear(); }
 
-    void clear() {
-        mPts.clear();
-    }
+    bool isEmpty() const { return mPts.isEmpty(); }
+    int count() const { return mPts.count(); }
 
     void move(const int& from, const int& to) {
         mPts.move(from, to);
-    }
-
-    template <class T, typename... Args>
-    T *createNewPt(const int& id, Args && ...args) {
-        const auto newPt = SPtrCreateTemplated(T)(args...);
-        mPts.insert(id, newPt);
-        return newPt.get();
-    }
-
-    template <class T, typename... Args>
-    T *createNewPt(Args && ...args) {
-        return createNewPt<T>(mPts.count(), args...);
     }
 private:
     QList<stdsptr<MovablePoint>> mPts;
