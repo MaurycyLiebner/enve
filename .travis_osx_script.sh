b681c3d7fa6943705276f17ec80039ffd6f00582
Maurycy Liebner <maurycy.liebner@gmail.com>
2020-04-16 15:10:07 +0200

    Try actually building enve for osx.

---
+++ .travis_osx_script.sh
@@ -0,0 +1,15 @@
+cd build
+cd Release
+# embed commit hash and date
+LATEST_COMMIT_HASH=\"$(git log --pretty=format:'%h' -n 1)\"
+enveSplash=../../src/app/GUI/envesplash.h
+echo "#ifndef LATEST_COMMIT_HASH" >> $enveSplash
+echo "#define LATEST_COMMIT_HASH $LATEST_COMMIT_HASH" >> $enveSplash
+echo "#endif" >> $enveSplash
+LATEST_COMMIT_DATE=\"$(git log -1 --format=%ai)\"
+echo "#ifndef LATEST_COMMIT_DATE" >> $enveSplash
+echo "#define LATEST_COMMIT_DATE $LATEST_COMMIT_DATE" >> $enveSplash
+echo "#endif" >> $enveSplash
+qmake ../../enve.pro
+make -j 2
+cd ..
