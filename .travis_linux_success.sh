e0cf645f26781c3554581cfce848bfefc6739cbd
Maurycy Liebner <maurycy.liebner@gmail.com>
2020-05-06 13:15:31 +0200

    Change GitHub CI release description. Fix.

--- .travis_linux_success.sh
+++ .travis_linux_success.sh
@@ -6,5 +6,5 @@ export LOCAL=$(git rev-parse HEAD);
 if [[ "$REMOTE" != "$LOCAL" ]]; then echo "Build no longer current. $REMOTE vs $LOCAL - aborting upload."; exit 0; fi;
 wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh;
 export UPLOADTOOL_SUFFIX="linux";
-export UPLOADTOOL_BODY=\"$(git log -1 --format=%ai)\";
+export UPLOADTOOL_BODY=$(git log -1 --pretty='%ad' --date=format:'%m/%d/%Y');
 bash upload.sh enve*.AppImage*;
--- .travis_osx_success.sh
+++ .travis_osx_success.sh
@@ -6,7 +6,7 @@ export LOCAL=$(git rev-parse HEAD);
 if [[ "$REMOTE" != "$LOCAL" ]]; then echo "Build no longer current. $REMOTE vs $LOCAL - aborting upload."; exit 0; fi;
 wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh;
 export UPLOADTOOL_SUFFIX="osx";
-export UPLOADTOOL_BODY=\"$(git log -1 --format=%ai)\";
+export UPLOADTOOL_BODY=$(git log -1 --pretty='%ad' --date=format:'%m/%d/%Y');
 export VERSION=$(git rev-parse --short HEAD)
 mv ./enve.dmg ./enve-$VERSION-x86_64.dmg 
 bash upload.sh enve*.dmg*;
